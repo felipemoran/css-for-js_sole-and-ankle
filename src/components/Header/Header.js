d2d0d2222af5105910a1c4a3eb75ae8535c2332a
Felipe Morán <felipe.moran11@gmail.com>
2021-11-03 23:20:25 +0000

    Header styled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import {COLORS, WEIGHTS} from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 
 const Header = () => {
-  // Our site features two visual headers, but they should be
-  // grouped semantically as a single header.
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Logo />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-      </MainHeader>
-    </header>
-  );
+    // Our site features two visual headers, but they should be
+    // grouped semantically as a single header.
+    return (
+        <header>
+            <SuperHeader/>
+            <MainHeader>
+                <Side>
+                    <Logo/>
+                </Side>
+                <Nav>
+                    <NavLink href="/sale">Sale</NavLink>
+                    <NavLink href="/new">New&nbsp;Releases</NavLink>
+                    <NavLink href="/men">Men</NavLink>
+                    <NavLink href="/women">Women</NavLink>
+                    <NavLink href="/kids">Kids</NavLink>
+                    <NavLink href="/collections">Collections</NavLink>
+                </Nav>
+                <Side/>
+            </MainHeader>
+        </header>
+    );
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 18px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  height: 72px;
+  display: flex;
+  gap: 48px;
+  align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -44,5 +55,9 @@ const NavLink = styled.a`
     color: ${COLORS.secondary};
   }
 `;
+
+const Side = styled.div`
+  flex: 1;
+`
 
 export default Header;
